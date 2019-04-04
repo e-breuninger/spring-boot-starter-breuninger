77a7acdfbd473f52be78d6fe1c2143758a15c022
Benedikt Stemmildt <benedikt@stemmildt.com>
2019-04-04 13:45:10 +0200

    chore(core): Dependency update

--- gradle/core.gradle.kts
+++ gradle/core.gradle.kts
@@ -1,7 +1,7 @@
 val coreVersions = mapOf(
   "kotlin" to "1.3.21",
-  "spring" to "5.1.5.RELEASE",
-  "spring-boot" to "2.1.3.RELEASE"
+  "spring" to "5.1.6.RELEASE",
+  "spring-boot" to "2.1.4.RELEASE"
 )
 
 extra["coreVersions"] = coreVersions
