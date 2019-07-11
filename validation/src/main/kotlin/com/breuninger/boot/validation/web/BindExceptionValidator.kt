a13cdabb3527e7bd219b0d17e5f6e6498a41a314
Benedikt Stemmildt <benedikt@stemmildt.com>
2019-07-11 15:55:18 +0200

    [validation] bugfix respond with 400 instead of 500 for invalid entities

--- validation/src/main/kotlin/com/breuninger/boot/validation/web/BindExceptionValidator.kt
+++ validation/src/main/kotlin/com/breuninger/boot/validation/web/BindExceptionValidator.kt
@@ -1,10 +1,12 @@
 package com.breuninger.boot.validation.web
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.validation.BeanPropertyBindingResult
 import org.springframework.validation.BindException
 import org.springframework.validation.Validator
+import org.springframework.web.bind.support.WebExchangeBindException
 
 @Component
 @ConditionalOnProperty(prefix = "breuni.validation", name = ["enabled"], havingValue = "true")
@@ -20,7 +22,8 @@ class BindExceptionValidator(private val webFluxValidator: Validator) {
   fun validateAndThrowException(target: Any) {
     val bindingResult = validate(target)
     if (bindingResult.hasErrors()) {
-      throw BindException(bindingResult)
+      val method = BindExceptionValidator::class.java.getDeclaredMethod("validateAndThrowException", Any::class.java)
+      throw WebExchangeBindException(MethodParameter.forExecutable(method, 0), bindingResult)
     }
   }
 }
