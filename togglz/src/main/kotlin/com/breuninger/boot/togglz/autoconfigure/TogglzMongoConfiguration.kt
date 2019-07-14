dc6b90f08398213148d018a7783e2bdf50bd93c4
Benedikt Stemmildt <benedikt@stemmildt.com>
2019-07-14 14:01:17 +0200

    [togglz] add ability to overwrite jobsMongoTemplate

--- togglz/src/main/kotlin/com/breuninger/boot/togglz/autoconfigure/TogglzMongoConfiguration.kt
+++ togglz/src/main/kotlin/com/breuninger/boot/togglz/autoconfigure/TogglzMongoConfiguration.kt
@@ -1,14 +1,18 @@
 package com.breuninger.boot.togglz.autoconfigure
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDbFactory
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.MongoConverter
 
 @Configuration
 @ConditionalOnProperty(prefix = "breuni.togglz", name = ["mongo.enabled"], havingValue = "true")
 class TogglzMongoConfiguration {
 
   @Bean
-  fun togglzMongoTemplate(mongoTemplate: MongoTemplate) = mongoTemplate
+  @ConditionalOnMissingBean
+  fun togglzMongoTemplate(mongoDbFactory: MongoDbFactory, converter: MongoConverter) = MongoTemplate(mongoDbFactory, converter)
 }
