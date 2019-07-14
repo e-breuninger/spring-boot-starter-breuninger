27c1b55a67c6cb186465afd1703c82af0ebb4035
Benedikt Stemmildt <benedikt@stemmildt.com>
2019-07-14 14:01:07 +0200

    [jobs] add ability to overwrite jobsMongoTemplate

--- jobs/src/main/kotlin/com/breuninger/boot/jobs/autoconfigure/JobsMongoConfiguration.kt
+++ jobs/src/main/kotlin/com/breuninger/boot/jobs/autoconfigure/JobsMongoConfiguration.kt
@@ -1,14 +1,18 @@
 package com.breuninger.boot.jobs.autoconfigure
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDbFactory
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.MongoConverter
 
 @Configuration
 @ConditionalOnProperty(prefix = "breuni.jobs", name = ["mongo.enabled"], havingValue = "true")
 class JobsMongoConfiguration {
 
   @Bean
-  fun jobsMongoTemplate(mongoTemplate: MongoTemplate) = mongoTemplate
+  @ConditionalOnMissingBean
+  fun jobsMongoTemplate(mongoDbFactory: MongoDbFactory, converter: MongoConverter) = MongoTemplate(mongoDbFactory, converter)
 }
